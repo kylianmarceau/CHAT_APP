63586ff5ce8be40403e762c04c2f0ab0c5d93d9f
Kylian Dabancourt <kyliandabancourt@5a-64-e5-c6-53-1a.lan.uct.ac.za>
2026-03-03 10:54:08 +0200

    dala

--- server.py
+++ server.py
@@ -19,6 +19,9 @@ def start():
     server.listen()
     while True:
         conn, addr = server.accept
+        thread = threading.Thread(target = handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1 }")
 
 
 print("[STARTING] server is starting .....")
