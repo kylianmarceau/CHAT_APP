3079b75aba20bb39934b0301efe5f12123b38f91
Kylian Dabancourt <kyliandabancourt@Kylians-MacBook-Air.local>
2026-03-03 09:52:40 +0200

    functional update

--- server.py
+++ server.py
@@ -6,3 +6,20 @@ import threading
 
 PORT = 6767
 SERVER = "196.42.116.252"
+ADDR = (SERVER, PORT)
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    pass
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept
+
+
+print("[STARTING] server is starting .....")
+start()
